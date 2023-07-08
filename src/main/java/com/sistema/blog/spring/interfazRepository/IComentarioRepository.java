8e721e88aebb2471e36cbddb8742a97a5f91ae6e
JuanPaM17 <saritha2514@hotmail.com>
2023-07-08 17:49:08 -0500

    Agregamos la dependencia ModelMapper

--- src/main/java/com/sistema/blog/spring/interfazRepository/IComentarioRepository.java
+++ src/main/java/com/sistema/blog/spring/interfazRepository/IComentarioRepository.java
@@ -1,6 +1,5 @@
 package com.sistema.blog.spring.interfazRepository;
 
-import com.sistema.blog.spring.dto.ComentarioDTO;
 import com.sistema.blog.spring.modelo.Comentario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
